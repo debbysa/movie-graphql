79cf866d80ba7d426b045305e98f9a856e195fe2
debbysa <deby.silvia23@gmail.com>
2019-09-27 09:53:15 +0700

    menambahkan actor, serta type relation antara movie dan actor

--- schema/schema.js
+++ schema/schema.js
@@ -1,21 +1,55 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 // var _ = require("underscore");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
 // dummy data
 var movies = [
-  { title: "harry potter", genre: "fantasy", id: "1" },
-  { title: "kamen riden", genre: "action", id: "2" },
-  { title: "naruto", genre: "fantasy", id: "3" }
+  { title: "harry potter", genre: "fantasy", id: "1", actorId: "2" },
+  { title: "kamen riden", genre: "action", id: "2", actorId: "1" },
+  { title: "naruto", genre: "fantasy", id: "3", actorId: "3" }
+];
+
+var actors = [
+  {
+    name: "John Black",
+    gender: "male",
+    id: "1"
+  },
+  {
+    name: "Jennyfer",
+    gender: "female",
+    id: "2"
+  },
+  {
+    name: "Del Luna",
+    gender: "female",
+    id: "3"
+  }
 ];
 
 const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    actor: {
+      type: ActorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(actors, { id: parent.actorId });
+      }
+    }
+  })
+});
+
+const ActorType = new GraphQLObjectType({
+  name: "Actor",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    gender: { type: GraphQLString }
   })
 });
 
@@ -24,10 +58,18 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db/ another source
+        console.log(typeof args.id);
         return _.find(movies, { id: args.id });
+      }
+    },
+    actors: {
+      type: ActorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(actors, { id: args.id });
       }
     }
   }
